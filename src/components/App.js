ae65407aa4cd4f63057fe321cc1f8358d6c8940f
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-16 21:26:19 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,20 @@ import './../styles/App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemList from "./ItemList";
 import ItemDetail from "./ItemDetail";
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<ItemList />} />
-      <Route path="/item/:id" element={<ItemDetail />} />
-    </Routes>
-  </BrowserRouter>
-);
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<ItemList />}/>
+        <Route path="/items/:id" element={<ItemDetail />}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  )
+}
 
 export default App
